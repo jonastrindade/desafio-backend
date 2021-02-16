ef20d92b963782860dbf5cb00bf0e0ae048b8276
jonastrindade <jonastrinda@gmail.com>
2021-02-15 23:42:30 -0300

    leitura do arquivo csv upado com active storage

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,5 +1,17 @@
 class Upload < ApplicationRecord
-  belongs_to :user
+  require 'smarter_csv'
+  # include Rails.application.routes.url_helpers
+  
+  # Callbacks
+  after_create_commit :create_data
 
+  # Relations
+  belongs_to :user
   has_one_attached :csv_file
+
+
+  def create_data
+    data = SmarterCSV.process(ActiveStorage::Blob.service.path_for(self.csv_file.key), {col_sep: ','})
+  end
+
 end
